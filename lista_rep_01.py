f5c2922f6cc35cc7430c1f7ebf207c8158d04dcb
Gabriel Herold <gabrielfilho2972@gmail.com>
2024-07-31 22:10:11 -0300

    lista_rep_01.py

--- lista_rep_01.py
+++ lista_rep_01.py
@@ -1,3 +1,6 @@
+# Faça um programa que peça uma nota, entre zero e dez. 
+# Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+
 nota = int(input('Digite uma nota de 0 até 10: '))
 
 while True:
